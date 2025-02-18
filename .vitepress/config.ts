7899c82a36248c9c14988d8cc52ed0a6dd43d16f
Nisekoi <49276902+Nisekoi5@users.noreply.github.com>
2025-02-18 13:18:27 +0800

    修复搜索框，更新依赖
    up
    
    f

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,8 +1,4 @@
-import process from 'node:process'
-import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links'
-import { UnlazyImages } from '@nolebase/markdown-it-unlazy-img'
-import { InlineLinkPreviewElementTransform } from '@nolebase/vitepress-plugin-inline-link-preview/markdown-it'
-
+import { presetMarkdownIt } from '@nolebase/integrations/vitepress/markdown-it'
 import { transformHeadMeta } from '@nolebase/vitepress-plugin-meta'
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
 // import { buildEndGenerateOpenGraphImages } from '@nolebase/vitepress-plugin-og-image/vitepress'
@@ -12,6 +8,8 @@ import { defineConfig } from 'vitepress'
 
 import { discordLink, githubRepoLink, siteDescription, siteName } from '../metadata'
 import head from './head'
+
+const nolebase = presetMarkdownIt()
 
 export default defineConfig({
   vue: {
@@ -30,6 +28,74 @@ export default defineConfig({
   description: siteDescription,
   ignoreDeadLinks: true,
   head,
+  themeConfig: {
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                },
+              },
+            },
+          },
+        },
+
+        // Add title ang tags field in frontmatter to search
+        // You can exclude a page from search by adding search: false to the page's frontmatter.
+        _render(src, env, md) {
+          // without `md.render(src, env)`, the some information will be missing from the env.
+          let html = md.render(src, env)
+          let tagsPart = ''
+          let headingPart = ''
+          let contentPart = ''
+          let fullContent = ''
+          const sortContent = () => [headingPart, tagsPart, contentPart] as const
+          let { frontmatter, content } = env
+
+          if (!frontmatter)
+            return html
+
+          if (frontmatter.search === false)
+            return ''
+
+          contentPart = content ||= src
+
+          const headingMatch = content.match(/^# .*/m)
+          const hasHeading = !!(headingMatch && headingMatch[0] && headingMatch.index !== undefined)
+
+          if (hasHeading) {
+            const headingEnd = headingMatch.index! + headingMatch[0].length
+            headingPart = content.slice(0, headingEnd)
+            contentPart = content.slice(headingEnd)
+          }
+          else if (frontmatter.title) {
+            headingPart = `# ${frontmatter.title}`
+          }
+
+          const tags = frontmatter.tags
+          if (tags && Array.isArray(tags) && tags.length)
+            tagsPart = `Tags: #${tags.join(', #')}`
+
+          fullContent = sortContent().filter(Boolean).join('\n\n')
+
+          html = md.render(fullContent, env)
+
+          return html
+        },
+      },
+    },
+  },
   locales: {
     root: {
       lang: 'zh-CN',
@@ -54,77 +120,12 @@ export default defineConfig({
         },
         sidebar: calculateSidebar([
           { folderName: 'zh-CN/笔记', separate: true },
+          { folderName: 'zh-CN/编目 Catalog', separate: true },
         ], 'zh-CN'),
         footer: {
           message: '用 <span style="color: #e25555;">&#9829;</span> 撰写',
           copyright:
         '<a class="footer-cc-link" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> © 2022-PRESENT Nólëbase 的创作者们',
-        },
-        search: {
-          provider: 'local',
-          options: {
-            locales: {
-              root: {
-                translations: {
-                  button: {
-                    buttonText: '搜索文档',
-                    buttonAriaLabel: '搜索文档',
-                  },
-                  modal: {
-                    noResultsText: '无法找到相关结果',
-                    resetButtonTitle: '清除查询条件',
-                    footer: {
-                      selectText: '选择',
-                      navigateText: '切换',
-                    },
-                  },
-                },
-              },
-            },
-
-            // Add title ang tags field in frontmatter to search
-            // You can exclude a page from search by adding search: false to the page's frontmatter.
-            _render(src, env, md) {
-              // without `md.render(src, env)`, the some information will be missing from the env.
-              let html = md.render(src, env)
-              let tagsPart = ''
-              let headingPart = ''
-              let contentPart = ''
-              let fullContent = ''
-              const sortContent = () => [headingPart, tagsPart, contentPart] as const
-              let { frontmatter, content } = env
-
-              if (!frontmatter)
-                return html
-
-              if (frontmatter.search === false)
-                return ''
-
-              contentPart = content ||= src
-
-              const headingMatch = content.match(/^# .*/m)
-              const hasHeading = !!(headingMatch && headingMatch[0] && headingMatch.index !== undefined)
-
-              if (hasHeading) {
-                const headingEnd = headingMatch.index! + headingMatch[0].length
-                headingPart = content.slice(0, headingEnd)
-                contentPart = content.slice(headingEnd)
-              }
-              else if (frontmatter.title) {
-                headingPart = `# ${frontmatter.title}`
-              }
-
-              const tags = frontmatter.tags
-              if (tags && Array.isArray(tags) && tags.length)
-                tagsPart = `Tags: #${tags.join(', #')}`
-
-              fullContent = sortContent().filter(Boolean).join('\n\n')
-
-              html = md.render(fullContent, env)
-
-              return html
-            },
-          },
         },
       },
     },
@@ -135,16 +136,12 @@ export default defineConfig({
       dark: 'one-dark-pro',
     },
     math: true,
+    preConfig: async (md) => {
+      await nolebase.install(md)
+    },
     config: (md) => {
       md.use(MarkdownItFootnote)
       md.use(MarkdownItMathjax3)
-      md.use(BiDirectionalLinks({
-        dir: process.cwd(),
-      }))
-      md.use(UnlazyImages(), {
-        imgElementTag: 'NolebaseUnlazyImg',
-      })
-      md.use(InlineLinkPreviewElementTransform)
     },
   },
   async transformHead(context) {
